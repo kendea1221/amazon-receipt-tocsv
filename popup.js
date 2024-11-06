213bf20a7859368ad0dfe15b67da69f567566e6c
kendea1221 <kenshirotamata@gmail.com>
2024-11-06 13:43:41 +0900

    UPD

--- popup.js
+++ popup.js
@@ -6,9 +6,9 @@ document.getElementById("export").addEventListener("click", async () => {
         func: () => {
             const data = [["Order ID"]]; // CSVのヘッダーとして"Order ID"を設定
 
-            document.querySelectorAll(".a-box-innner").forEach(order => {
-                // <bdi dir="ltr">と<span class="a-color-secondary" dir="ltr">の要素をそれぞれ取得
-                const orderIdElements = order.querySelectorAll("bdi[dir='ltr'], span[class='a-color-secondary'][dir='ltr']");
+            document.querySelectorAll(".order").forEach(order => {
+                // <span class="a-color-secondary" dir="ltr"> のみを取得
+                const orderIdElements = order.querySelectorAll("span.a-color-secondary[dir='ltr']");
                 
                 orderIdElements.forEach(orderIdElement => {
                     const orderId = orderIdElement.innerText.trim() || "N/A";
@@ -19,7 +19,7 @@ document.getElementById("export").addEventListener("click", async () => {
             const csvContent = data.map(e => e.join(",")).join("\n");
             const link = document.createElement("a");
             link.href = URL.createObjectURL(new Blob([csvContent], { type: "text/csv" }));
-            link.download = "amazon_order_ids.csv";
+            link.download = "amazon_order.csv";
             link.click();
         }
     });
